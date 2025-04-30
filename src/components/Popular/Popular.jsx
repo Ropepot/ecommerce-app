ff81b5d714c81b74d7cd13992a5144b659cdd44d
riconaljon <riconaljon@gmail.com>
2025-05-01 04:05:11 +0800

    Worked on items

---
+++ src/components/Popular/Popular.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+import "./Popular.css"
+import data_product from '../assets/Assets/Frontend_Assets/data'
+
+const Popular = () => {
+    return(
+        <div className="popular">
+            <h1>POPULAR IN WOMEN</h1>
+            <hr />
+        </div>
+    )
+}
+
+export default Popular
